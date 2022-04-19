1140963a003d68779ab6f6faff34872eb47d236a
Zach G <zjgordy@gmail.com>
2022-04-18 20:47:55 -0700

    3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './navbar/navbar.component';
 
 function App() {
   return (
     <div className="App">
-     
+     <Navbar />
     </div>
   );
 }
--- src/navbar/navbar.component.js
+++ src/navbar/navbar.component.js
@@ -1,26 +1,29 @@
-import React from 'react'
+import './navbar.styles.css'
 
 const Navbar = () => {
-  return (
-    <div className=''> 
+    return (
+        <div className='navbar-container'>
+            <div className='logo-container'>
+                <a>[ Opserver ]</a>
+            </div>
+            <div className='links-container'>
+                <a className='navbar-link'>
+                    How To
+                </a>
+                <a className='navbar-link'>
+                    Tech Skills
+                </a>
+                <a className='navbar-link'>
+                    Reads
+                </a>
+            
+            <div className='sign-in'>
+                Sign In
+            </div>
+            </div>
 
-    <div>Opserver</div>
-
-    <span className=''>
-
-    </span>
-    <span className=''>
-
-    </span>
-    <span className=''>
-
-    </span>
-    <div className=''>
-        Sign In
-    </div>
-
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Navbar
