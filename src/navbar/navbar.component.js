cf38274a58f64bae12259eae9277c60f5168f260
Zach G <zjgordy@gmail.com>
2022-04-18 20:29:30 -0700

    2nd

---
+++ src/navbar/navbar.component.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className=''> 
+
+    <div>Opserver</div>
+
+    <span className=''>
+
+    </span>
+    <span className=''>
+
+    </span>
+    <span className=''>
+
+    </span>
+    <div className=''>
+        Sign In
+    </div>
+
+    </div>
+  )
+}
+
+export default Navbar
